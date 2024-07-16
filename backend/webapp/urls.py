77a42e7bf924afe7a70cd4a11fc7adb1015b9790
mohimishra016 <mohimishra016@gmail.com>
2024-07-15 23:37:52 -0400

    initial commit

---
+++ backend/webapp/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path
+# from .views import users_list, users_detail
+
+# urlpatterns = [
+#     path('contact/', users_list.as_view() ),
+#     path('contact/<int:pk>/', users_detail.as_view())
+# ]
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+const Header = () => {
+  return (
+    <div className="text-center">
+      <h1>App with React + Django</h1>
+    </div>
+  );
+};
+export default Header;
